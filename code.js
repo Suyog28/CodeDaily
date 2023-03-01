84f470803a81a8a27d4f42a6faf24b382987eb9d
Suyog28 <suyogvmuley@gmail.com>
2023-03-01 16:07:26 +0530

    Promises

--- code.js
+++ code.js
@@ -2058,3 +2058,63 @@ function process(picture) {
 let url = "https://wwww.javascripttutorial.net/pic.jpg";
 
 download(url, process);
+
+
+//Promises All Method
+
+
+// const p1 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     console.log("The First promise has resolved");
+//     resolve(10);
+//   }, 1 * 1000);
+// });
+
+// const p2 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     console.log("The Second promise has resolved");
+//     resolve(20);
+//   }, 2 * 1000);
+// });
+
+// const p3 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     console.log("The third promise has resolved");
+//     resolve(30);
+//   }, 3 * 1000);
+// });
+
+// Promise.all([p1, p2, p3]).then((results) => {
+//   const total = results.reduce((a, b) => a + b);
+//   console.log(`Results:${results}`);
+//   console.log(`Total:${total}`);
+// })
+
+
+
+//Rejecting Promise 
+
+const p1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log("The First promise has resolved");
+    resolve(10);
+  }, 1 * 1000);
+});
+
+const p2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log("The Second promise has failed");
+    reject('Failed');
+  }, 2 * 1000);
+});
+
+const p3 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log("The third promise has resolved");
+    resolve(30);
+  }, 3 * 1000);
+});
+
+Promise.all([p1, p2, p3])
+  .then(console.log)
+  .catch(console.log);
