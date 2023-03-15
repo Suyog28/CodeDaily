009c0030f5a0974f0421ca01d78148d4d786644d
Suyog28 <suyogvmuley@gmail.com>
2023-03-15 22:18:38 +0530

    15 Javascript Question solve

--- code.js
+++ code.js
@@ -2485,19 +2485,19 @@ console.log(vowelsFind("CodeHelp"));
 //For example, given the arrays[1, 2, 3, 4, 5] and[3, 4, 5, 6, 7], the function should return [3, 4, 5].
 //Hint: You may need to use nested loops and conditional statements to solve this problem.
 
-function findCommonElement(arr1, arr2) {
-  let commonElement = [];
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr2.includes(arr1[i])) {
-      if (!commonElement.includes(arr1[i])) {
-        commonElement.push(arr1[i]);
-      }
-    }
-  }
+// function findCommonElement(arr1, arr2) {
+//   let commonElement = [];
+//   for (let i = 0; i < arr1.length; i++) {
+//     if (arr2.includes(arr1[i])) {
+//       if (!commonElement.includes(arr1[i])) {
+//         commonElement.push(arr1[i]);
+//       }
+//     }
+//   }
 
-  commonElement.sort((a, b) => a - b);
-  return commonElement;
-}
+//   commonElement.sort((a, b) => a - b);
+//   return commonElement;
+// }
 
 const arr1 = [1, 2, 3, 4, 5];
 const arr2 = [3, 4, 5, 6, 74];
