1422101f258b90c1da594a5358e664ac017903c1
Suyog Muley <53749026+Suyog28@users.noreply.github.com>
2023-08-29 11:04:46 +0000

    Conversation

---
+++ 01_Basic_Js/03_conversionOperations.js
@@ -0,0 +1,20 @@
+// *************Conversion**********
+
+let score = 33;
+let score2 = "33abc"
+
+console.log(typeof(score));
+console.log(typeof(score2));
+
+let stringValue = Number(score2);
+console.log(typeof(stringValue));
+console.log(stringValue);
+
+console.log(typeof(stringValue));
+
+let numtoString = String(score);
+
+console.log(typeof(numtoString));
+
+// *************Operations*************
+
