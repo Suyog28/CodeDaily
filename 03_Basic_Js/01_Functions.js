e3d6ae6395a67ffc0879d476f215e7755792182d
Suyog Muley <53749026+Suyog28@users.noreply.github.com>
2023-09-05 00:39:12 +0000

    functions

---
+++ 03_Basic_Js/01_Functions.js
@@ -0,0 +1,37 @@
+// console.log("S");
+// console.log("U");
+// console.log("Y");
+// console.log("O");
+// console.log("G");
+
+// function myName()
+// {
+//     console.log("S");
+// console.log("U");
+// console.log("Y");
+// console.log("O");
+// console.log("G");
+
+// }
+
+// myName();
+
+
+// function userLoggedIn(username){
+//    return `${username} just logged In`;
+// }
+
+// console.log(userLoggedIn());
+
+
+// function userLoggedIn(username){
+//    return `${username} just logged In`;
+// }
+
+// console.log(userLoggedIn("Suyog"));
+
+// function userLoggedIn(username = "sam"){
+//    return `${username} just logged In`;
+// }
+// console.log(userLoggedIn());
+
