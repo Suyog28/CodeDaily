db1178ffc3925f505f48d6b40b15fcc8e0f9e1f2
Suyog Muley <53749026+Suyog28@users.noreply.github.com>
2023-09-05 16:41:12 +0000

    IIFE and Control Flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,29 @@
+ //control Flow
+
+ //if Else condition
+
+ const temp = 41;
+
+//  if(temp<50){
+//     console.log("Temp is less than 41");
+//  } 
+
+ // <, >, <=, >=, ==, === , && , || 
+
+ const balance =1000;
+
+//  if(balance< 750){
+//     console.log("Bal is less than 750");
+//  } else  if(balance <500){
+//     console.log("Bal is less than 500");
+//  }else{
+//     console.log("Bal is grater than 1000");
+//  }
+
+
+const isLoggedIn = true;
+const isValid = true;
+
+// if(isLoggedIn && isValid && 2==2){
+//     console.log("Valid User");
+// }
