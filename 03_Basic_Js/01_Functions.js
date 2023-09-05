59f4aaab4700bc49e5c5efe38ca28b1d6b80d046
Suyog Muley <53749026+Suyog28@users.noreply.github.com>
2023-09-05 01:12:26 +0000

    functions with Object and Array

--- 03_Basic_Js/01_Functions.js
+++ 03_Basic_Js/01_Functions.js
@@ -35,3 +35,51 @@
 // }
 // console.log(userLoggedIn());
 
+function calculateCartPrice(...num1){
+return num1
+}
+
+const result = calculateCartPrice(2, 2, 5, 5);
+console.log(result);
+
+let sum=0;
+result.forEach(Element=>{
+    sum += Element;
+  
+})
+console.log(sum);
+
+
+result.reduce((a,b)=>
+ sum= a+b
+)
+console.log(sum);
+
+
+//function with objects
+
+const user = {
+    name:"Suyog",
+    Age:23
+};
+
+function userDetails(userObj){
+    return `${userObj.name} is my name and Age is ${userObj.Age}`
+}
+
+// console.log(userDetails(user));
+ console.log(userDetails({
+    name:"Chaitali",
+    Age:18
+ }));
+
+
+ //Function with Array
+
+ const myArray = [200,500,600];
+
+ function newArray (arr){
+    return arr[0];
+ }
+
+ console.log(newArray(myArray));
