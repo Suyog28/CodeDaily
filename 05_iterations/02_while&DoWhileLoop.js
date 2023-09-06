b4a522d3d0c7a2606b995580a6b72d461defd29c
Suyog Muley <53749026+Suyog28@users.noreply.github.com>
2023-09-06 03:00:51 +0000

    While and Do While Loop

---
+++ 05_iterations/02_while&DoWhileLoop.js
@@ -0,0 +1,16 @@
+//While Loop
+
+let num = 1;
+
+// while (num <=10) {
+//     console.log(`Num Value is:${num}`);
+//     num = num +2;
+    
+// }
+
+//Do While loop - DO will run once 
+
+do {
+    console.log(num);
+    num = num +3;
+} while (num <=10);
