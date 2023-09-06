feaffe5c9372167b903e2c816a416af645f33acd
Suyog Muley <53749026+Suyog28@users.noreply.github.com>
2023-09-06 11:49:24 +0000

    ForEach Loop

---
+++ 05_iterations/05_forEachLoop.js
@@ -0,0 +1,31 @@
+//forEach Loop
+
+const names = ["Suyog", "Reliance", "DM", "RF"];
+
+names.forEach((names)=>{
+// console.log(names);
+})
+
+const languages = [
+    {
+        lName : "Javascript",
+        Donotions:"js"
+
+    },
+    {
+        lName : "PHP",
+        Donotions:"PHP"
+
+    },
+    {
+        lName : "React",
+        Donotions:"jsx"
+
+    }
+    
+]
+
+languages.forEach((items)=>{
+    console.log(items.lName);
+})
+
