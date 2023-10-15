29109baa249815efefd6db3238aa3ed0312d06d8
Suyog28 <suyogvmuley@gmail.com>
2023-10-15 21:17:00 +0530

    Teaching to the Student

---
+++ TeachingJS.js
@@ -0,0 +1,82 @@
+// alert("Suyog Chaitu")
+
+const person = ["suyog", "chaitu", "rajesh", "sagar"];
+
+let list = [
+    { text: 'bacon', cost: 12, need: true },
+    { text: 'eggs', cost: 10, need: true },
+    { text: 'butter', cost: 5, need: false },
+    { text: 'milk', cost: 4, need: false },
+    { text: 'bread', cost: 3, need: true }
+];
+
+// console.log(typeof person);
+//Iterating
+
+// for (let i = 0; i <= list.length - 1; i++) {
+//     console.log(list[i].cost);
+// }
+
+// list.forEach(function (item) {
+
+//     console.log(item.cost);
+// }
+// )
+
+
+//Arrow function
+
+//list.forEach(item => console.log(item.text))
+
+// for (const iterator of list) {
+//     console.log(iterator.text);
+// }
+
+// for (const iterator in list) {
+//     console.log(iterator);
+// }
+
+//Searching
+
+// let index = person.indexOf("sagar");
+// console.log(index);
+// let index1 = person.lastIndexOf("sagar");
+// console.log(index1);
+
+//Finding
+
+// let index = person.find(item => item == "sagar");
+// console.log(index);
+
+
+//String Manipulation
+
+// let husbund = "suyog"
+// let spouse = "chaitu"
+
+// console.log(husbund + " and " + spouse);
+// console.log(`${husbund} and ${spouse}`);
+// console.log(husbund.concat(" ", spouse));
+
+//Splice Method
+
+// list.splice(2, 0, "suyog");
+
+//Slice Method
+
+console.log(person.slice(0, 3));
+
+
+//Map Method
+
+
+
+
+//Filter Method
+
+
+//Reduce Method
+
+
+
+
